ce2eff5bf2cd7d37eff3bf70d583dd1237f9e92a
songjoon <songjoon000@gmail.com>
2025-10-10 20:39:01 +0900

    removed suffix of build-editable

--- cmeel/consts.py
+++ cmeel/consts.py
@@ -3,10 +3,12 @@ import os
 import sys
 
 # vvv Warning: keep sync with cmeel_pth.py
-CMEEL_PREFIX = "cmeel.prefix"
-SITELIB = os.path.join(  # noqa: PTH118
-    "lib",
-    "python" + ".".join(sys.version.split(".")[:2]),
-    "site-packages",
-)
+# CMEEL_PREFIX = "cmeel.prefix"
+CMEEL_PREFIX = ""
+# SITELIB = os.path.join(  # noqa: PTH118
+#     "lib",
+#     "python" + ".".join(sys.version.split(".")[:2]),
+#     "site-packages",
+# )
+SITELIB = ""
 # ^^^
